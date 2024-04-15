47dcc3f7ce4aa53b658cea7ebdf82b90df2824ec
Adhy Wiranata <adhywiranatap@gmail.com>
2024-04-15 20:24:32 +0700

    chore: test barebone cloudflare page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+// import { Hono } from "hono";
+// import { serveStatic } from "hono/cloudflare-workers";
+// import { renderServerRoute } from "../servers/handlers";
+
+// const app = new Hono();
+
+// app.use("/app/*", serveStatic({ root: "./" }));
+// app.get("/", (c) => {
+//   return renderServerRoute(c);
+// });
+
+// export default app;
 import { Hono } from "hono";
-// import { serveStatic } from "@hono/node-server/serve-static";
-import { renderServerRoute } from "../servers/handlers";
+import { renderer } from "./renderer";
 
 const app = new Hono();
 
-// app.use("/app/*", serveStatic({ root: "./" }));
+app.get("*", renderer);
+
 app.get("/", (c) => {
-  return renderServerRoute(c);
+  return c.render(<h1>Hello, Cloudflare Pages!</h1>);
 });
 
 export default app;
